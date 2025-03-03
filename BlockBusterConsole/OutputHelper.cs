fc79dd740e9e0ca61e677340fba51534abe9a63c
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2025-03-02 21:08:55 -0500

    Finished BlockBlusterConsole Program file

---
+++ BlockBusterConsole/OutputHelper.cs
@@ -0,0 +1,41 @@
+﻿using BlockBuster.Models;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using CsvHelper;
+
+namespace BlockBusterConsole
+{
+    public class OutputHelper
+    {
+
+
+        public void WriteToConsole(List<Movie> movies)
+        {
+            Console.WriteLine("List of Movies");
+            foreach (var m in movies)
+            {
+                Console.WriteLine($"Movie ID: {m.MovieId} Title: {m.Title} Release Year:{m.ReleaseYear}");
+            }
+        }
+
+        public void WriteToCSV(List<Movie> movies)
+        {
+            using (var writer = new StreamWriter(@"..\file.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(movies);
+            }
+        }
+
+        // Overloaded method to accept a string message
+        public void WriteToConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ BlockBusterConsole/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using BlockBuster;
+using System.Collections.Generic;
+using BlockBusterConsole;
+using BlockBuster.Models;
+
+/*
+var b = BlockBusterBasicFunctions.GetAllMovies();
+var oh = new OutputHelper();
+oh.WriteToCSV(b);
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2) // 2 args required
+        {
+            Console.WriteLine("Usage:BlockBusterConsole.exe <OutputType> <MethodName> [Parameter]");
+            return;
+        }
+
+        string outputType = args[0]; // "Console" or "CSV"
+        string methodName = args[1]; // Ex. GetMovieByTitle
+        string parameter = args.Length > 2 ? args[2] : null; //
+
+        OutputHelper oh = new OutputHelper();
+        List<Movie> result = new List<Movie>();
+
+        using (var db = new Se407BlockBusterContext())
+        {
+            switch (methodName)
+            {
+
+                case "GetMovieByTitle":
+                    if (string.IsNullOrWhiteSpace(parameter))
+                    {
+                        Console.WriteLine("Error: Missing movie title.");
+                        return;
+                    }
+                    var movie = db.Movies.FirstOrDefault(m => m.Title == parameter);
+                    if (movie != null) result.Add(movie);
+                    break;
+
+                case "GetAllMovies":
+                    result = db.Movies.ToList();
+                    break;
+
+                default:
+                    Console.WriteLine("Error: Unknown method name.");
+                    return;
+            }
+        }
+
+        if (outputType.Equals("CSV"))
+        {
+            oh.WriteToCSV(result);
+            oh.WriteToConsole("Output written to: file.csv");
+        }
+        else if (outputType.Equals("Console"))
+        {
+            oh.WriteToConsole(result);
+        }
+        else
+        {
+            Console.WriteLine("Error: Invalid output type. Please type 'CSV' or 'Console.'");
+        }
+    }
+}
+
+
+
+
+
+
+
